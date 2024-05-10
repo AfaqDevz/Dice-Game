60bff0e8825f01fb01a7a843dece29c10ea1f9c8
Afaq <154227525+AfaqDevz@users.noreply.github.com>
2024-05-10 22:46:29 +0500

    Updated Dice-Game

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let diceimage = document.getElementById('diceimg')
 let diceimage2 = document.getElementById('diceimg2')
 let wintext = document.getElementById('winh1')
 let rollbutton = document.getElementById('rollbut')
+let scoretext = document.getElementById('score')
+let score = 0;
 
 function rolldice () {
     let random = Math.floor(Math.random()*6)
@@ -13,10 +15,20 @@ function rolldice () {
     diceimage.src = imagesdice[random]
     diceimage2.src = imagesdice[random2]
     rollbutton.style.background = "#0080ff";
+    wintext.classList.remove("animate__tada");
+    diceimage.classList.add("animate__shakeY");
+    diceimage2.classList.add("animate__shakeY");
+    setTimeout(() => { diceimage.classList.remove("animate__shakeY"); }, 500);
+    setTimeout(() => { diceimage2.classList.remove("animate__shakeY"); }, 500);
+
+
     if (diceimage.src == diceimage2.src){
         wintext.innerText = 'YOU WIN!'
+        wintext.classList.add("animate__tada");
         wintext.style.color = "#15ff00";
         rollbutton.innerText = 'Restart';
         rollbutton.style.background = "#00bb10";
+        score++
+        scoretext.innerText = score;
     }
 }
